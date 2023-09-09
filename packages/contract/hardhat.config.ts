608bc40796b4d8a690c4ff4a7fd1a9054a845780
tayutaedomo <tayutaedomo@gmail.com>
2023-09-09 21:13:54 +0900

    Section 2 Lesson 3 is completed

--- packages/contract/hardhat.config.ts
+++ packages/contract/hardhat.config.ts
@@ -1,8 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+if (process.env.TEST_ACCOUNT_PRIVATE_KEY === undefined) {
+  console.log("private key is missing");
+}
+
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    local: {
+      url: process.env.TEST_RPC_URL || "",
+      gasPrice: 225000000000,
+      chainId: 321123,
+      accounts:
+        process.env.TEST_ACCOUNT_PRIVATE_KEY !== undefined
+          ? [process.env.TEST_ACCOUNT_PRIVATE_KEY]
+          : [],
+    },
+  },
 };
 
 export default config;
