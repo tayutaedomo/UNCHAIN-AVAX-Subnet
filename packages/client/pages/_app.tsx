1ab9790e2e9a0bb0e474e4edcc9bcad3177ee678
tayutaedomo <tayutaedomo@gmail.com>
2023-09-12 01:46:06 +0900

    Section 3 Lesson 2 is completed

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { CurrentAccountProvider } from '../context/CurrentAccountProvider';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <CurrentAccountProvider>
+      <Component {...pageProps} />
+    </CurrentAccountProvider>
+  );
 }
+
+export default MyApp;
--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,7 +1,15 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next';
+
+import Layout from '../components/Layout/Layout';
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <Layout>
+      <div className="bg-home h-screen bg-cover bg-center flex justify-around">
+        <h1 className="text-6xl font-bold my-auto">AVAX Subnet X Finance</h1>
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
